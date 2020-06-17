3c1609d5e36a65423ecd0437c6abc9e457e3cda5
bearjam <sam.croce@gmail.com>
2020-06-17 16:20:30 -0400

    update

--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var Spotify = require('node-spotify-api'); //Using the Spotify api and getting the key from keys.js
+var Spotify = require('node-spotify-api'); 
 var spotify = new Spotify(keys.spotify);
 
-var moment = require('moment'); //Both required to use moment for node
+var moment = require('moment'); 
 moment().format();
 
-var axios = require('axios'); //To get the information from the APIs for movie and concert-this
+var axios = require('axios'); 
 
-var fs = require('fs'); //To read the random.txt file for the do-what-it-says function
+var fs = require('fs'); 
 
-var command = process.argv[2]; //For the switch statement
-var value = process.argv[3]; //To send the song/movie/concert to their respective functions
+var command = process.argv[2]; 
+var value = process.argv[3]; 
 
 switch (command) {
     case "concert-this":
@@ -23,9 +23,6 @@ switch (command) {
         break;
     case "movie-this":
         movieThis(value);
-        break;
-    case "do-what-it-says":
-        doThis(value);
         break;
 };
 
@@ -99,13 +96,4 @@ function movieThis(value) {
     
 }
 
-function doThis(value) {
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        if (error) {
-            return console.log(error);
-        }
-        var dataArr = data.split(',');
-        spotifySong(dataArr[0], dataArr[1]);
-    })
-}
